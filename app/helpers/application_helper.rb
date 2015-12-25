81b669e1e6466d22eb5ee6eb55ed9bfac9b7f0e4
Juan Bausa <juanbausa@gmail.com>
2015-12-25 08:59:01 +0100

    Fixed messages from devise 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,14 @@ module ApplicationHelper
 end
 
   def flash_messages(opts = {})
+    #flash.except(:timedout).each do |msg_type, message|
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", style: "margin-top:10px;") do 
-              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-              concat message 
-            end)
+      if message != true
+        concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in", style: "margin-top:10px;") do 
+                concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+                concat message 
+              end)
+      end
     end
     nil
   end
