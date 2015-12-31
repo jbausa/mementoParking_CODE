7e5cbb352038ed5e3571d77fd3fb84507e8f3ac8
Juan Bausa <juanbausa@gmail.com>
2015-12-31 16:45:01 +0100

    Prueba mobile 22

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-require 'rack/cache'
-
-if !Rails.env.development? && !Rails.env.test?
-  config.middleware.insert_before Rack::Cache, Rack::Static, urls: [config.assets.prefix],     root: 'public'
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
